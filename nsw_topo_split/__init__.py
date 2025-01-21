627f10ae3382e9401bb321342462dcf3f689663d
tschanzer <76029848+tschanzer@users.noreply.github.com>
2025-01-21 20:01:18 +1100

    Refactor and add command line options

--- nsw_topo_split/__init__.py
+++ nsw_topo_split/__init__.py
@@ -1,10 +1,8 @@
-"""A package for splitting NSW topographic maps into A3 pages"""
+"""A package for splitting NSW topographic maps across smaller pages"""
 
 import copy
-import http.client
 import importlib.resources
 import json
-import pathlib
 import urllib.request
 from typing import Iterator
 
@@ -14,106 +12,219 @@ URL_PREFIX = (
     'https://portal.spatial.nsw.gov.au/download/NSWTopographicMaps/'
     'DTDB_GeoReferenced_Raster_CollarOn_161070'
 )
-
 MM_PER_PT = 25.4/72
 COVER_WIDTH_PT = 326
-OVERLAP_MM = 20
-N_PAGES_X = 3
-N_PAGES_Y = 2
-PAGE_HEIGHT_PT = pypdf.PaperSize.A3.width
-PAGE_WIDTH_PT = pypdf.PaperSize.A3.height
 
-name_dict = json.load(
-        importlib.resources.open_text('nsw_topo_split', 'names_25k.json'))
-
-
-def map_filename(name: str) -> str:
-    """
-    Convert a map name to its filename, e.g. 'katoomba' -> '8930-1S+KATOOMBA'.
-    """
-
-    return name_dict[name]
+map_names_scales: dict[str, dict[str, str]] = json.load(
+    importlib.resources.open_text(
+        'nsw_topo_split', 'nsw_topo_map_names_scales.json'))
 
 
 def download_map(name: str, year: str, out: str) -> None:
     """
-    Downloads a 1:25k NSW topo map.
+    Download a NSW topo map.
 
     Args:
         name: The lowercase name of the map, e.g. 'kanangra'.
-        year: The publication year of the map, e.g. '2017' or '2022'.
+        year: The publication year, e.g. '2017' or '2022'.
         out: Path for saving the downloaded file.
     """
 
-    url = URL_PREFIX + '/' + year + '/25k/' + map_filename(name) + '.pdf'
+    full_name = map_names_scales[name]['full_name']
+    scale = map_names_scales[name]['scale']
+    url = '/'.join([URL_PREFIX, year, scale, full_name]) + '.pdf'
     with urllib.request.urlopen(url) as stream:
         with open(out, 'wb') as f:
             f.write(stream.read())
 
 
-def _mm_to_pt(x_mm: float) -> float:
+def mm_to_pt(x_mm: float) -> float:
+    """Convert millimetres to points."""
     return x_mm/MM_PER_PT
 
 
-def make_cover_page(page: pypdf.PageObject) -> pypdf.PageObject:
+def crop_hide_translate(
+        page: pypdf.PageObject,
+        *,
+        left: float = 0.,
+        right: float = 0.,
+        bottom: float = 0.,
+        top: float = 0.,
+    ) -> pypdf.PageObject:
     """
-    Put the map cover page and legend side-by-side on a portrait page.
+    Crop a page, white out the cropped content and translate to (0,0).
+
+    Args:
+        page: Page to be cropped.
+        left, right, bottom, top: Amount to remove from each side, in
+            points (default 0).
+
+    Returns:
+        A new page, with the content cropped by the specified amounts,
+        and translated so the bottom left corner of the visible content
+        at (0,0).
     """
 
-    cover = copy.copy(page)
-    cover.mediabox.left = cover.mediabox.right - COVER_WIDTH_PT
-    cover.mediabox.top /= 2
-    cover_page = pypdf.PageObject.create_blank_page(
-        width=cover.mediabox.width, height=cover.mediabox.height)
-    cover_page.merge_page(cover)
-    cover_page.mediabox.left = (
-        cover.mediabox.left + (cover.mediabox.width*2 - PAGE_HEIGHT_PT)/2)
-    cover_page.mediabox.right = cover_page.mediabox.left + PAGE_HEIGHT_PT
-    cover_page.mediabox.bottom = (
-        cover.mediabox.bottom + (cover.mediabox.height - PAGE_WIDTH_PT)/2)
-    cover_page.mediabox.top = cover_page.mediabox.bottom + PAGE_WIDTH_PT
+    page = copy.deepcopy(page)
+    page.mediabox.left += left
+    page.mediabox.right -= right
+    page.mediabox.bottom += bottom
+    page.mediabox.top -= top
 
-    legend = copy.copy(page)
-    legend_page = pypdf.PageObject.create_blank_page(
-        width=legend.mediabox.width, height=legend.mediabox.height)
-    legend.mediabox.left = legend.mediabox.right - COVER_WIDTH_PT
-    legend.mediabox.bottom = legend.mediabox.top/2
-    legend_page.merge_page(legend)
+    # Put the bottom left corner of the content at (0,0)
     transformation = pypdf.Transformation().translate(
-        tx=legend.mediabox.width, ty=-legend.mediabox.height)
-    legend_page.add_transformation(transformation, expand=True)
+        tx=-page.mediabox.left, ty=-page.mediabox.bottom)
+    page.add_transformation(transformation, expand=True)
 
-    cover_page.merge_page(legend_page)
-    return cover_page
+    # Merge onto a new blank page to white out cropped content
+    new_page = pypdf.PageObject.create_blank_page(
+        width=page.mediabox.width, height=page.mediabox.height)
+    new_page.merge_page(page)
+    return new_page
 
 
-def make_map_pages(page: pypdf.PageObject) -> Iterator[pypdf.PageObject]:
+def make_cover(original: pypdf.PageObject) -> pypdf.PageObject:
     """
-    Split the map across landscape pages.
+    Put the map title page and legend side-by-side.
+
+    Args:
+        original: The original map page.
+
+    Returns:
+        A new page with the map title page and legend side-by-side.
     """
 
-    map_ = copy.copy(page)
-    page = pypdf.PageObject.create_blank_page(
-        width=map_.mediabox.width, height=map_.mediabox.height)
-    map_.mediabox.right -= COVER_WIDTH_PT
-    page.merge_page(map_)
+    # Grab the title page (bottom right corner of the original map)
+    cover = crop_hide_translate(
+        original,
+        left=original.mediabox.width - COVER_WIDTH_PT,
+        top=original.mediabox.height/2,
+    )
 
-    overlap_pt = _mm_to_pt(OVERLAP_MM)
+    # Grab the legend (top right corner of the original map)
+    legend = crop_hide_translate(
+        original,
+        left=original.mediabox.width - COVER_WIDTH_PT,
+        bottom=original.mediabox.height/2,
+    )
+    # Move it to the right so it can sit beside the title page
+    transformation = pypdf.Transformation().translate(tx=COVER_WIDTH_PT)
+    legend.add_transformation(transformation, expand=True)
+
+    cover.merge_page(legend, expand=True)
+    return cover
+
+
+def split_page(
+        page: pypdf.PageObject,
+        page_size: tuple[float, float],
+        n_pages: tuple[int, int],
+        overlap: tuple[float, float],
+        no_white_space: bool,
+    ) -> Iterator[pypdf.PageObject]:
+    """
+    Split a page across several smaller pages.
+
+    Args:
+        page: The page to be split.
+        page_size: (width, height) of the output pages in points.
+        n_pages: Number of pages (nx, ny) in each direction.
+        overlaps: (overlap_x, overlap_y) between pages in points.
+        no_white_space: If True, increase overlaps to eliminate any
+            white space on the output pages.
+
+    Yields:
+        Output pages in column-major order.
+    """
+
+    # Work out the total dimensions of the multi-page layout (accounting
+    # for overlaps)
     layout_dims = (
-        N_PAGES_X*PAGE_WIDTH_PT - (N_PAGES_X-1)*overlap_pt,
-        N_PAGES_Y*PAGE_HEIGHT_PT - (N_PAGES_Y-1)*overlap_pt,
+        n_pages[0]*page_size[0] - (n_pages[0]-1)*overlap[0],
+        n_pages[1]*page_size[1] - (n_pages[1]-1)*overlap[1],
     )
+    # If the layout would be larger than the map in either direction,
+    # increase the corresponding overlap so this is no longer the case
+    if no_white_space:
+        if layout_dims[0] > page.mediabox.width and n_pages[0] > 1:
+            layout_dims = (page.mediabox.width, layout_dims[1])
+            overlap = (
+                (n_pages[0]*page_size[0] - layout_dims[0])/(n_pages[0] - 1),
+                overlap[1],
+            )
+        if layout_dims[1] > page.mediabox.height and n_pages[1] > 1:
+            layout_dims = (layout_dims[0], page.mediabox.height)
+            overlap = (
+                overlap[0],
+                (n_pages[1]*page_size[1] - layout_dims[1])/(n_pages[1] - 1),
+            )
+
+    # Work out where to put the bottom left corner of the bottom left
+    # page so that the layout is centred on the map
     layout_origin = (
-        map_.mediabox.left + (map_.mediabox.width - layout_dims[0])/2,
-        map_.mediabox.bottom + (map_.mediabox.height - layout_dims[1])/2,
+        (page.mediabox.width - layout_dims[0])/2,
+        (page.mediabox.height - layout_dims[1])/2,
     )
 
-    for j in range(N_PAGES_X):
+    # Produce the output pages in column-major order
+    for j in range(n_pages[0]):
         page.mediabox.left = (
-            layout_origin[0] + j*(PAGE_WIDTH_PT - overlap_pt))
-        page.mediabox.right = page.mediabox.left + PAGE_WIDTH_PT
-        for i in range(N_PAGES_Y - 1, -1, -1):
+            layout_origin[0] + j*(page_size[0] - overlap[0]))
+        page.mediabox.right = page.mediabox.left + page_size[0]
+        for i in range(n_pages[1] - 1, -1, -1):
             page.mediabox.bottom = (
-                layout_origin[1] + i*(PAGE_HEIGHT_PT - overlap_pt))
-            page.mediabox.top = page.mediabox.bottom + PAGE_HEIGHT_PT
+                layout_origin[1] + i*(page_size[1] - overlap[1]))
+            page.mediabox.top = page.mediabox.bottom + page_size[1]
             yield page
+
+
+def make_cover_pages(
+        original: pypdf.PageObject,
+        page_size: tuple[float, float],
+        n_pages: tuple[int, int],
+        overlap: tuple[float, float],
+        no_white_space: bool,
+    ) -> Iterator[pypdf.PageObject]:
+    """
+    Make a multi-page cover.
+
+    Args:
+        original: The original map page.
+        page_size: (width, height) of the output pages in points.
+        n_pages: Number of pages (nx, ny) in each direction.
+        overlaps: (overlap_x, overlap_y) between pages in points.
+        no_white_space: If True, increase overlaps to eliminate any
+            white space on the output pages.
+
+    Yields:
+        Output pages in column-major order.
+    """
+
+    cover = make_cover(original)
+    yield from split_page(cover, page_size, n_pages, overlap, no_white_space)
+
+
+def make_map_pages(
+        original: pypdf.PageObject,
+        page_size: tuple[float, float],
+        n_pages: tuple[int, int],
+        overlap: tuple[float, float],
+        no_white_space: bool,
+    ) -> Iterator[pypdf.PageObject]:
+    """
+    Make a multi-page cover.
+
+    Args:
+        original: The original map page.
+        page_size: (width, height) of the output pages in points.
+        n_pages: Number of pages (nx, ny) in each direction.
+        overlaps: (overlap_x, overlap_y) between pages in points.
+        no_white_space: If True, increase overlaps to eliminate any
+            white space on the output pages.
+
+    Yields:
+        Output pages in column-major order.
+    """
+
+    map_ = crop_hide_translate(original, right=COVER_WIDTH_PT)
+    yield from split_page(map_, page_size, n_pages, overlap, no_white_space)
--- nsw_topo_split/__main__.py
+++ nsw_topo_split/__main__.py
@@ -1,4 +1,4 @@
-"""A script for splitting NSW topographic maps into A3 pages."""
+"""A script for splitting NSW topographic maps across smaller pages."""
 
 import argparse
 import pathlib
@@ -7,45 +7,129 @@ import pypdf
 
 from nsw_topo_split import (
     download_map,
-    make_cover_page,
+    make_cover_pages,
     make_map_pages,
-    map_filename
+    map_names_scales,
+    mm_to_pt
 )
 
 
-def main():
-    """Split a NSW topographic map into A3 pages."""
+def main() -> None:
+    """Split a NSW topographic map across smaller pages."""
 
     parser = argparse.ArgumentParser(
-        description='Split a NSW topographic map into A3 pages')
-    parser.add_argument('name', help='Lowercase map name, e.g. kanangra')
-    parser.add_argument('year', help='Year of publication')
+        description='split a NSW topographic map across smaller pages')
+    parser.add_argument(
+        'mode',
+        choices=['map', 'cover'],
+        help="'map' to make the map pages, 'cover' to make the cover pages",
+    )
+    parser.add_argument('name', help='lowercase map name, e.g. kanangra')
+    parser.add_argument('year', help='year of publication')
     parser.add_argument(
         '-o',
         '--out',
         type=pathlib.Path,
         default=pathlib.Path.cwd(),
-        help='Output directory (default: current working directory)',
+        help=(
+            'output directory (default: working directory). '
+            'Files are output in a subdirectory corresponding to the '
+            'publication year and map name, e.g., 2022/katoomba.'
+        ),
+    )
+    parser.add_argument(
+        '-s', '--size', default='A3', help='page size (e.g., A4; default A3)'
+    )
+    parser.add_argument(
+        '-p',
+        '--portrait',
+        action='store_true',
+        help='use portrait layout rather than landscape',
+    )
+    parser.add_argument(
+        '-n',
+        '--n-pages',
+        type=int,
+        nargs=2,
+        help=(
+            'horizontal and vertical number of pages '
+            '(default: [4, 3] for A4, [3, 2] otherwise)'
+        ),
+    )
+    parser.add_argument(
+        '-l',
+        '--overlap',
+        type=float,
+        nargs=2,
+        default=[20., 20.],
+        help=(
+            'horizontal and vertical overlap between pages in mm '
+            '(default: [20, 20])'
+        ),
+    )
+    parser.add_argument(
+        '-w',
+        '--allow-white-space',
+        action='store_true',
+        help='do not expand overlaps to eliminate white space',
+    )
+    parser.add_argument(
+        '-f',
+        '--force-download',
+        action='store_true',
+        help=(
+            'download the original map, even if it already exists '
+            'in the output directory'
+        ),
     )
     args = parser.parse_args()
 
-    out = (args.out/map_filename(args.name)).with_suffix('.pdf')
-    download_map(args.name, args.year, out)
+    full_name = map_names_scales[args.name]['full_name']
+    page_size = getattr(pypdf.PaperSize, args.size)
+    if not args.portrait:
+        page_size = (page_size[1], page_size[0])
+    if args.n_pages is None:
+        match args.size, args.mode:
+            case 'A4', 'cover':
+                args.n_pages = [1, 2]
+            case 'A4', 'map':
+                args.n_pages = [4, 3]
+            case 'A3', 'cover':
+                args.n_pages = [1, 1]
+            case 'A3', 'map':
+                args.n_pages = [3, 2]
+            case _:
+                raise RuntimeError(
+                    '-n/--n-pages must be specified for paper sizes '
+                    'other than A4 and A3'
+                )
 
-    reader = pypdf.PdfReader(out)
-    writer = pypdf.PdfWriter()
-    writer.add_page(make_cover_page(reader.pages[0]))
-    out = (args.out/(map_filename(args.name) + '_cover')).with_suffix('.pdf')
-    with open(out, 'wb') as f:
-        writer.write(f)
+    out_dir = args.out/args.year/args.name
+    out_dir.mkdir(parents=True, exist_ok=True)
+    master_file = (out_dir/full_name).with_suffix('.pdf')
+    if args.force_download or not master_file.exists():
+        download_map(args.name, args.year, master_file)
+
+    reader = pypdf.PdfReader(master_file)
+    if args.mode == 'cover':
+        make_pages = make_cover_pages
+        out_file = (out_dir/(full_name + '_cover')).with_suffix('.pdf')
+    else:
+        make_pages = make_map_pages
+        out_file = (out_dir/(full_name + '_split')).with_suffix('.pdf')
 
     writer = pypdf.PdfWriter()
-    for page in make_map_pages(reader.pages[0]):
+    pages = make_pages(
+        reader.pages[0],
+        page_size,
+        args.n_pages,
+        (mm_to_pt(args.overlap[0]), mm_to_pt(args.overlap[1])),
+        not args.allow_white_space,
+    )
+    for page in pages:
         writer.add_page(page)
-    out = (args.out/(map_filename(args.name) + '_split')).with_suffix('.pdf')
-    with open(out, 'wb') as f:
+    with open(out_file, 'wb') as f:
         writer.write(f)
-
     reader.close()
 
 
